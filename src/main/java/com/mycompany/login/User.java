70e9e8b7bb16b3eb02b4f670c6042d0355c70ecc
ST10514561 <ST10514561@myemeris.edu.za>
2026-04-13 22:19:59 +0200

    Add files via upload
    My Final product

---
+++ src/main/java/com/mycompany/login/User.java
@@ -0,0 +1,42 @@
+
+package com.mycompany.login;
+
+
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String cellPhoneNumber;
+    private String secretAnswer;
+    
+    //
+    public User(String username, String password, String email,String secretCellPhoneNumber, String secretAnswer){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.secretcellPhoneNumber = secretcellPhoneNumber ;
+    }
+    
+    //Getters and Setters
+    public String getPassword(){
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getSecretcellPhoneNumber(){
+        return name;
+    }
+    
+    //
+    public void setPassword(String password){
+        this.password = password;
+        
+    }
+    
+   
+           
+    
+            
+    
+}
